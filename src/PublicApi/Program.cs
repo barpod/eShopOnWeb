c2351164601f07db82b4275fa4ecf6e709ca4301
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2023-04-14 10:42:55 +0200

    Break Program.cs

--- src/PublicApi/Program.cs
+++ src/PublicApi/Program.cs
@@ -51,139 +51,139 @@ var configSection = builder.Configuration.GetRequiredSection(BaseUrlConfiguratio
 builder.Services.Configure<BaseUrlConfiguration>(configSection);
 var baseUrlConfig = configSection.Get<BaseUrlConfiguration>();
 
-builder.Services.AddMemoryCache();
-
-var key = Encoding.ASCII.GetBytes(AuthorizationConstants.JWT_SECRET_KEY);
-builder.Services.AddAuthentication(config =>
-{
-    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(config =>
-{
-    config.RequireHttpsMetadata = false;
-    config.SaveToken = true;
-    config.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-
-const string CORS_POLICY = "CorsPolicy";
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: CORS_POLICY,
-        corsPolicyBuilder =>
-        {
-            corsPolicyBuilder.WithOrigins(
-                baseUrlConfig!.WebBase.Replace("host.docker.internal", "localhost").TrimEnd('/'),
-                "https://eshoponweb-secondary-app-service.azurewebsites.net",
-                "https://eshoponweb-primary-app-service-staging.azurewebsites.net",
-                "https://eshoponweb-primary-app-service.azurewebsites.net" );
-            corsPolicyBuilder.AllowAnyMethod();
-            corsPolicyBuilder.AllowAnyHeader();
-        });
-});
-
-builder.Services.AddControllers();
-builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
-builder.Configuration.AddEnvironmentVariables();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-    c.EnableAnnotations();
-    c.SchemaFilter<CustomSchemaFilters>();
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-            {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header,
-
-                        },
-                        new List<string>()
-                    }
-            });
-});
-
-var app = builder.Build();
-
-app.Logger.LogInformation("PublicApi App created...");
-
-app.Logger.LogInformation("Seeding Database...");
-
-using (var scope = app.Services.CreateScope())
-{
-    var scopedProvider = scope.ServiceProvider;
-    try
-    {
-        var catalogContext = scopedProvider.GetRequiredService<CatalogContext>();
-        await CatalogContextSeed.SeedAsync(catalogContext, app.Logger);
-
-        var userManager = scopedProvider.GetRequiredService<UserManager<ApplicationUser>>();
-        var roleManager = scopedProvider.GetRequiredService<RoleManager<IdentityRole>>();
-        var identityContext = scopedProvider.GetRequiredService<AppIdentityDbContext>();
-        await AppIdentityDbContextSeed.SeedAsync(identityContext, userManager, roleManager);
-    }
-    catch (Exception ex)
-    {
-        app.Logger.LogError(ex, "An error occurred seeding the DB.");
-    }
-}
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseCors(CORS_POLICY);
-
-app.UseAuthorization();
-
-// Enable middleware to serve generated Swagger as a JSON endpoint.
-app.UseSwagger();
-
-// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
-// specifying the Swagger JSON endpoint.
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-});
-
-app.MapControllers();
-app.MapEndpoints();
-
-app.Logger.LogInformation("LAUNCHING PublicApi");
-app.Run();
-
 throw new Exception ("Cannot move further 2");
+// builder.Services.AddMemoryCache();
 
-public partial class Program { }
+// var key = Encoding.ASCII.GetBytes(AuthorizationConstants.JWT_SECRET_KEY);
+// builder.Services.AddAuthentication(config =>
+// {
+//     config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+// })
+// .AddJwtBearer(config =>
+// {
+//     config.RequireHttpsMetadata = false;
+//     config.SaveToken = true;
+//     config.TokenValidationParameters = new TokenValidationParameters
+//     {
+//         ValidateIssuerSigningKey = true,
+//         IssuerSigningKey = new SymmetricSecurityKey(key),
+//         ValidateIssuer = false,
+//         ValidateAudience = false
+//     };
+// });
+
+// const string CORS_POLICY = "CorsPolicy";
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy(name: CORS_POLICY,
+//         corsPolicyBuilder =>
+//         {
+//             corsPolicyBuilder.WithOrigins(
+//                 baseUrlConfig!.WebBase.Replace("host.docker.internal", "localhost").TrimEnd('/'),
+//                 "https://eshoponweb-secondary-app-service.azurewebsites.net",
+//                 "https://eshoponweb-primary-app-service-staging.azurewebsites.net",
+//                 "https://eshoponweb-primary-app-service.azurewebsites.net" );
+//             corsPolicyBuilder.AllowAnyMethod();
+//             corsPolicyBuilder.AllowAnyHeader();
+//         });
+// });
+
+// builder.Services.AddControllers();
+// builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+// builder.Configuration.AddEnvironmentVariables();
+
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen(c =>
+// {
+//     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+//     c.EnableAnnotations();
+//     c.SchemaFilter<CustomSchemaFilters>();
+//     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//     {
+//         Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+//                       Enter 'Bearer' [space] and then your token in the text input below.
+//                       \r\n\r\nExample: 'Bearer 12345abcdef'",
+//         Name = "Authorization",
+//         In = ParameterLocation.Header,
+//         Type = SecuritySchemeType.ApiKey,
+//         Scheme = "Bearer"
+//     });
+
+//     c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+//             {
+//                     {
+//                         new OpenApiSecurityScheme
+//                         {
+//                             Reference = new OpenApiReference
+//                             {
+//                                 Type = ReferenceType.SecurityScheme,
+//                                 Id = "Bearer"
+//                             },
+//                             Scheme = "oauth2",
+//                             Name = "Bearer",
+//                             In = ParameterLocation.Header,
+
+//                         },
+//                         new List<string>()
+//                     }
+//             });
+// });
+
+// var app = builder.Build();
+
+// app.Logger.LogInformation("PublicApi App created...");
+
+// app.Logger.LogInformation("Seeding Database...");
+
+// using (var scope = app.Services.CreateScope())
+// {
+//     var scopedProvider = scope.ServiceProvider;
+//     try
+//     {
+//         var catalogContext = scopedProvider.GetRequiredService<CatalogContext>();
+//         await CatalogContextSeed.SeedAsync(catalogContext, app.Logger);
+
+//         var userManager = scopedProvider.GetRequiredService<UserManager<ApplicationUser>>();
+//         var roleManager = scopedProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//         var identityContext = scopedProvider.GetRequiredService<AppIdentityDbContext>();
+//         await AppIdentityDbContextSeed.SeedAsync(identityContext, userManager, roleManager);
+//     }
+//     catch (Exception ex)
+//     {
+//         app.Logger.LogError(ex, "An error occurred seeding the DB.");
+//     }
+// }
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseDeveloperExceptionPage();
+// }
+
+// app.UseMiddleware<ExceptionMiddleware>();
+
+// app.UseHttpsRedirection();
+
+// app.UseRouting();
+
+// app.UseCors(CORS_POLICY);
+
+// app.UseAuthorization();
+
+// // Enable middleware to serve generated Swagger as a JSON endpoint.
+// app.UseSwagger();
+
+// // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+// // specifying the Swagger JSON endpoint.
+// app.UseSwaggerUI(c =>
+// {
+//     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+// });
+
+// app.MapControllers();
+// app.MapEndpoints();
+
+// app.Logger.LogInformation("LAUNCHING PublicApi");
+// app.Run();
+
+
+// public partial class Program { }
