86c51ac12cd1abd0c9902559515d576856843e8b
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2023-04-18 20:39:13 +0200

    Call the function

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
@@ -6,6 +8,8 @@ using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Services;
 
@@ -13,16 +17,19 @@ public class OrderService : IOrderService
 {
     private readonly IRepository<Order> _orderRepository;
     private readonly IUriComposer _uriComposer;
+    private readonly IConfiguration _configuration;
     private readonly IRepository<Basket> _basketRepository;
     private readonly IRepository<CatalogItem> _itemRepository;
 
     public OrderService(IRepository<Basket> basketRepository,
         IRepository<CatalogItem> itemRepository,
         IRepository<Order> orderRepository,
-        IUriComposer uriComposer)
+        IUriComposer uriComposer,
+        IConfiguration configuration)
     {
         _orderRepository = orderRepository;
         _uriComposer = uriComposer;
+        _configuration = configuration;
         _basketRepository = basketRepository;
         _itemRepository = itemRepository;
     }
@@ -47,6 +54,17 @@ public class OrderService : IOrderService
         }).ToList();
 
         var order = new Order(basket.BuyerId, shippingAddress, items);
+
+        using(var client = new HttpClient())
+        {
+            client.BaseAddress = new System.Uri("https://eshoponweb-orderitemreserver-function-app.azurewebsites.net");
+            client.DefaultRequestHeaders.Add("x-functions-key",_configuration["master_order_item_reserver_key"]);
+            var json = JsonConvert.SerializeObject(order);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var blobExtensionKey = _configuration["blob_extension_key"];
+            var requestUri = $"api/OrderItemsReserver/{order.Id}?code={blobExtensionKey}&clientId=blobs_extension";
+            await client.PostAsync(requestUri, data);
+        }
 
         await _orderRepository.AddAsync(order);
     }
