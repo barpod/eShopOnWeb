e79ebf9b575ca489ce2adb8ca05de838bba0c9cb
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2023-04-26 17:49:02 +0200

    add cosmos db output binding

--- src/OrderItemsReserver.Function/OrderItemsReserverFunction.cs
+++ src/OrderItemsReserver.Function/OrderItemsReserverFunction.cs
@@ -3,10 +3,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.Text;
 
 namespace OrderItemsReserver;
 
@@ -15,13 +14,15 @@ public static class OrderItemsReserverFunction
     [FunctionName("OrderItemsReserver")]
     public static async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = "OrderItemsReserver/{orderId}")] HttpRequest req,
-        [Blob("order-reservation-container/order-{orderId}.json", FileAccess.Write)] Stream orderIdBlob,
+        [CosmosDB(
+                databaseName: "ToDoItems",
+                containerName: "cosmosdbsqlcontainer",
+                Connection = "CosmosDBConnection")]IAsyncCollector<dynamic> documentsOut,
         string orderId,
         ILogger log)
     {
         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        var bytes = Encoding.UTF8.GetBytes(requestBody);
-        await orderIdBlob.WriteAsync(bytes);
+        await documentsOut.AddAsync(new { Order = requestBody, id = orderId });
         return new OkObjectResult("Order id " + orderId);
     }
 }
